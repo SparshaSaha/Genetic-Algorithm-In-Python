63077b7f869a17ec685e96b397e82522d3d9a4d4
sparsha <saha.sparsha@gmail.com>
2019-06-17 01:14:34 +0530

    Config file modified

--- Config.py
+++ Config.py
@@ -1,10 +1,11 @@
 class Config(object):
 
     def __init__(self):
-        self.NO_OF_GENES = 4
+        self.NO_OF_GENES = 14
         self.MUTATION_PROBABILITY = 0.1
-        self.POPULATION_SIZE = 16
-        self.IDEAL_FITNESS = 4
+        self.POPULATION_SIZE = 200
+        self.IDEAL_FITNESS = 14
         self.ELITE_CARRY_OVER = 2
         self.FITNESS_CATEGORY = "maximize"
-        self.CROSSOVER_PROBABILITY = 0.9
+        self.CROSSOVER_PROBABILITY = 0.6
+        self.GENE_TYPE = "binary"
