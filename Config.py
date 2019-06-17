9f06930ecfba7dc1682e40467929413b3d138028
sparsha <saha.sparsha@gmail.com>
2019-06-17 08:18:23 +0530

    config modified

--- Config.py
+++ Config.py
@@ -4,8 +4,8 @@ class Config(object):
         self.NO_OF_GENES = 14
         self.MUTATION_PROBABILITY = 0.1
         self.POPULATION_SIZE = 200
-        self.IDEAL_FITNESS = 14
+        self.IDEAL_FITNESS = 0
         self.ELITE_CARRY_OVER = 2
-        self.FITNESS_CATEGORY = "maximize"
+        self.FITNESS_CATEGORY = "minimize"
         self.CROSSOVER_PROBABILITY = 0.6
         self.GENE_TYPE = "binary"
